27382367c33afcef0c5d2f7a0feb7e1dbca3453b
李腾 <296575985@qq.com>
2018-07-27 18:00:54 +0800

    输出打包

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
-
+    var btn = document.createElement('button');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -14,6 +15,9 @@ function component() {
     myIcon.src = Icon;
     element.appendChild(myIcon);
 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  // entry: './src/index.js',
+    entry: {
+     app: './src/index.js',
+     print: './src/print.js'
+    },
+    plugins: [
+        // 插件功能,我们可以通过HtmlWebpackPlugin生成新的html文件替换原有的
+     new HtmlWebpackPlugin({
+       title: 'Output Management'
+   }),
+   new CleanWebpackPlugin(['dist']), // 清除dist下的文件
+   ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         // 这部分是loader 用来处理引入任何其他类型的文件.
