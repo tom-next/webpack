790d47a85d1e1575965666173f56a62320ebf858
tom <tom.next.miao@gmail.com>
2018-08-18 00:00:47 +0800

    json配置

--- webpack3/src/entry.js
+++ webpack3/src/entry.js
@@ -5,3 +5,7 @@ let w = "hello"
 const q = "#@42"
 document.getElementById('title').innerHTML = "Hello wewew tom"
 console.log($, $("#title")[0].innerHTML = "1213");
+
+var json = require("../config.json")
+
+document.getElementById('json').innerHTML = json.name
